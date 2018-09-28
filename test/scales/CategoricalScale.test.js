f8119b9275ad306d08558450bbb1e895cb4ba3db
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2018-09-28 16:06:39 -0400

    Toggle stack button

--- test/scales/CategoricalScale.test.js
+++ test/scales/CategoricalScale.test.js
@@ -8,7 +8,6 @@ test('able to create a CategoricalScale', () => {
     expect(scale.domainFiltered.length).toBe(6);
 });
 
-
 test('able to get a color from a CategoricalScale', () => {
     let scale = new CategoricalScale("sample_id", "Samples", ["S1", "S2", "S3", "S4", "S5", "S6"]);
     expect(scale.color("S1").substring(0, 3)).toBe("rgb");
