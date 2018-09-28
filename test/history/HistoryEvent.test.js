85595eca3205f1e800356ba392a7a7df608f3fc6
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2018-09-28 11:21:30 -0400

    Travis test coverage bug fix

--- test/history/HistoryEvent.test.js
+++ test/history/HistoryEvent.test.js
@@ -21,3 +21,16 @@ test('able to check for a non-related HistoryEvent', () => {
     let other = new HistoryEvent(HistoryEvent.types.SCALE, "signature", "filter", [[6, 7, 8, 9]]);
     expect(event.isRelated(other)).toBe(false);
 });
+
+test('can serialize HistoryEvent using toJson method', () => {
+    let event = new HistoryEvent(HistoryEvent.types.SCALE, "sample_id", "zoom", [2, 5]);
+    let eventJson = JSON.stringify(event.toJson());
+    let expectedJson = JSON.stringify({
+        "type": HistoryEvent.types.SCALE,
+        "id": "sample_id",
+        "action": "zoom",
+        "params": [2, 5]
+    })
+
+    expect(eventJson).toBe(expectedJson);
+});
